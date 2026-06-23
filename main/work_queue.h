8f52816d63240fff636d58db2b026a0051426cff
bugoao <46293847+bugoao@users.noreply.github.com>
2026-06-23 19:01:46 +0800

    Update work_queue.h

--- main/work_queue.h
+++ main/work_queue.h
@@ -4,8 +4,8 @@
 #include <pthread.h>
 #include "mining.h"
 
-#define QUEUE_SIZE 5120
-#define QUEUE_LOW_WATER_MARK_MAX 5120
+#define QUEUE_SIZE 512
+#define QUEUE_LOW_WATER_MARK_MAX 512
 
 typedef struct
 {
